f89f555abda429c1ad944f26c35fb5f1a5e3db20
ibdevelopyn <devyngray@proton.me>
2025-01-19 13:44:51 -0500

    start working on generic query for entities

--- examples/invoice/main.go
+++ examples/invoice/main.go
@@ -2,29 +2,85 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"time"
+
 	"github.com/djcopley/quickbooks-sdk-go"
 	"github.com/djcopley/quickbooks-sdk-go/invoice"
 	"golang.org/x/oauth2"
-	"log"
 )
 
 func main() {
 	qbOAuthConfig := &oauth2.Config{
-		ClientID:     "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-		ClientSecret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		ClientID:     "",
+		ClientSecret: "",
 		Scopes:       []string{"com.intuit.quickbooks.accounting"},
 		Endpoint: oauth2.Endpoint{
 			TokenURL: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer",
 			AuthURL:  "https://appcenter.intuit.com/connect/oauth2",
 		},
-		RedirectURL: "localhost:8080/oauth/callback",
+		RedirectURL: "http://localhost:8080/oauth/callback",
 	}
+
+	// // use PKCE to protect against CSRF attacks
+	// verifier := oauth2.GenerateVerifier()
+
+	// url := qbOAuthConfig.AuthCodeURL(
+	// 	"state",
+	// 	oauth2.AccessTypeOffline,
+	// 	oauth2.S256ChallengeOption(verifier),
+	// )
+
+	// ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	// defer cancel()
+
+	// eg, ctx := errgroup.WithContext(ctx)
+
+	// listener, err := net.Listen("tcp4", "localhost:8080")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// eg.Go(func() error {
+	// 	defer listener.Close()
+
+	// 	<-ctx.Done()
+	// 	return ctx.Err()
+	// })
+
+	// eg.Go(func() error {
+	// 	mux := http.NewServeMux()
+
+	// 	mux.HandleFunc("/oauth/callback", func(w http.ResponseWriter, r *http.Request) {
+	// 		queryParams := r.URL.Query()
+	// 		code := queryParams.Get("code")
+	// 		realmId := queryParams.Get("realmId")
+	// 		// requestId := queryParams.Get("state")
+
+	// 		tok, err := qbOAuthConfig.Exchange(ctx, code, oauth2.VerifierOption(verifier))
+	// 		if err != nil {
+	// 			w.WriteHeader(http.StatusInternalServerError)
+	// 		}
+
+	// 		fmt.Println(realmId)
+	// 		fmt.Println(tok.AccessToken)
+	// 		fmt.Println(tok.RefreshToken)
+
+	// 	})
+
+	// 	return http.Serve(listener, mux)
+	// })
+
+	// fmt.Println(url)
+
+	// eg.Wait()
+
 	t := &oauth2.Token{
 		AccessToken:  "",
 		RefreshToken: "",
+		Expiry:       time.Now(),
 	}
 
-	realmId := "xxxxxxxxxxxxxxxx"
+	realmId := ""
 
 	client := qbOAuthConfig.Client(context.Background(), t)
 
@@ -34,27 +90,15 @@ func main() {
 		realmId,
 	)
 
-	obj := &invoice.Invoice{
-		Line: []invoice.Line{
-			{
-				DetailType: "SalesItemLineDetail",
-				SalesItemLineDetail: invoice.SalesItemLineDetail{
-					ItemRef: invoice.ItemRef{
-						Name:  "Services",
-						Value: "1",
-					},
-				},
-				Amount: 100.0,
-			},
-		},
-		CustomerRef: invoice.CustomerRef{
-			Value: "1",
-		},
+	// _, err := quickbooks.NewQuery[account.Account](qbClient).All()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	invoices, err := quickbooks.NewQuery[invoice.Invoice](qbClient).All()
+	if err != nil {
+		panic(err)
 	}
 
-	var response invoice.Response
-	err := qbClient.CreateEntity(obj, &response)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(invoices)
 }
